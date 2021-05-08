8e027ed9845c7d324290a83ebd23e78d81fa405e
gmarcha <gmarcha@github.com>
2021-05-08 22:39:10 +0200

    up
    up

--- tester.sh
+++ tester.sh
@@ -12,7 +12,7 @@ if [ -z $1 ]; then
 	printf "\t\t-a, --all: both of them\n"
 	exit 1
 fi
-if [ $1 != "mand" ] && [ $1 != "bonus" ] && [ $1 != "all" ]; then
+if [ $1 != "mand" ] && [ $1 != "bonus" ] && [ $1 != "all" ] && [ $1 != "m" ] && [ $1 != "b" ] && [ $1 != "a" ]; then
 	printf "error: invalid args.\n"
 	exit 1
 fi
