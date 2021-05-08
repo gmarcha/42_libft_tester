0280668faafc2aee7ec39d4c02a3c51cc5a954f0
gmarcha <gmarcha@github.com>
2021-05-08 20:36:54 +0200

    up
    42_libft_tester.sh:update

--- 42_libft_tester.sh
+++ 42_libft_tester.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
 SEP="========================================================================================================================"
-MAND="ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_strlen.c ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_strncmp.c ft_strlcpy.c ft_strlcat.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c"
-BONUS="ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c"
-EXP="ft_csplit.c ft_itoa_base.c ft_strsjoin.c ft_ischarset.c ft_free_strs.c ft_nbrlen_base.c ft_strcat.c ft_strclen.c"
+# MAND="ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_strlen.c ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c ft_strncmp.c ft_strlcpy.c ft_strlcat.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c"
+# BONUS="ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c"
+FILES="../ft_*.c"
 
-FILES="$MAND $EXP"
-MAIN="main.c"
+if [ -z $1 ]; then
+	printf "error: no args.\n"
+	exit 1
+fi
+if [ $1 = "mand" ]; then
+	MAIN="main_mandatory.c mandatory.c"
+fi
+if [ $1 = "bonus" ]; then
+	MAIN="main_bonus.c bonus.c"
+fi
+if [ $1 = "all" ]; then
+	MAIN="main_all.c mandatory.c bonus.c"
+fi
 
 printf "\033[38;2;136;23;152;1;3m$SEP\n\n"
 echo "██╗  ██╗██████╗     ██╗     ██╗██████╗ ███████╗████████╗    ████████╗███████╗███████╗████████╗███████╗██████╗ "
