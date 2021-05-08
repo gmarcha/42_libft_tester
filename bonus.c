531fa717294866549f63ea8d07ec183010e99c25
gmarcha <gmarcha@github.com>
2021-05-08 22:40:32 +0200

    up
    multiple definition correction

--- bonus.c
+++ bonus.c
@@ -14,6 +14,7 @@
 #define HEADER(str)		getchar();printf_rgb("136;23;152;1", "\n"S3"\n\n%120s\n", str)
 #define END(str)		printf_rgb("136;23;152;3", "\n%120s\n%120s\n%120s\n", "Test finished.", "https://github.com/gmarcha", str)
 
+#undef	printf_rgb
 void	printf_rgb(char *rgb, char *format, ...)
 {
 	va_list			args;
