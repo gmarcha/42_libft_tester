228ce01311fb062e12e209d513b459fdac2ac134
gmarcha <gmarcha@github.com>
2021-05-09 15:13:29 +0200

    mand bonus wait inclusion

--- bonus.c
+++ bonus.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <ctype.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "../libft.h"
 
 #define S1				"========================================================================================================================"
