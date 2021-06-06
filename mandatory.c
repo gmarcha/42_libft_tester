9d3ffcdaf3b2522cb971cd1b140690cd0367b725
gmarcha <gmarcha@github.com>
2021-06-06 22:01:28 +0200

    update out compare

--- mandatory.c
+++ mandatory.c
@@ -18,6 +18,8 @@
 
 void	printf_rgb(char *rgb, char *format, ...);
 
+void	out_cmp(void);
+
 size_t	bsd_strlcpy(char * dst, const char * src, size_t maxlen) 
 {
 	const size_t srclen = strlen(src);
@@ -410,22 +412,7 @@ void	str_out_int_ret_cmp(char *test, char *user, size_t ret_test, size_t ret_use
 	}
 }
 
-void	out_cmp(void)
-{
-	int				fd_test = open("out_test", O_RDONLY);
-	int				fd_user = open("out_user", O_RDONLY);
-	char			ret_test[4096] = {0};
-	char			ret_user[4096] = {0};
 
-	read(fd_test, ret_test, 4096);
-	read(fd_user, ret_user, 4096);
-	if (strcmp(ret_test, ret_user) == 0)
-		printf_rgb("57;181;74", "$> OK!\n");
-	else
-		printf_rgb("222;56;43", "$> KO! invalid diff.\n");
-	close(fd_test);
-	close(fd_user);
-}
 
 /* ************************************************************************** */
 /*                                                                            */
--- utils.c
+++ utils.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 void	printf_rgb(char *rgb, char *format, ...)
 {
@@ -10,4 +13,21 @@ void	printf_rgb(char *rgb, char *format, ...)
 	vprintf(format, args);
 	va_end(args);
 	printf("\033[0m");
+}
+
+void	out_cmp(void)
+{
+	int				fd_test = open("out_test", O_RDONLY);
+	int				fd_user = open("out_user", O_RDONLY);
+	char			ret_test[4096] = {0};
+	char			ret_user[4096] = {0};
+
+	read(fd_test, ret_test, 4096);
+	read(fd_user, ret_user, 4096);
+	if (strcmp(ret_test, ret_user) == 0)
+		printf_rgb("57;181;74", "$> OK!\n");
+	else
+		printf_rgb("222;56;43", "$> KO! invalid diff.\n");
+	close(fd_test);
+	close(fd_user);
 }
